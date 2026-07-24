9040848b3d196f819c8580ff310054b8f493baa9
Pkowech <aaronkipkoechrono@outlook.com>
2026-07-24 19:45:53 +0300

    Update build.rs

--- rust_analytics/build.rs
+++ rust_analytics/build.rs
@@ -3,12 +3,13 @@ use std::path::PathBuf;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     let proto_file = [
+        PathBuf::from("/protos/analytics.proto"),  // Docker mount path
         manifest_dir.join("../protos/analytics.proto"),
         manifest_dir.join("protos/analytics.proto"),
     ]
     .into_iter()
     .find(|path| path.exists())
-    .unwrap_or_else(|| manifest_dir.join("../protos/analytics.proto"));
+    .unwrap_or_else(|| PathBuf::from("/protos/analytics.proto"));
 
     let include_dir = proto_file.parent().unwrap_or(&manifest_dir);
     let proto_file_for_build = proto_file.clone();
